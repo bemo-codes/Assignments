b5e89f5e99cb6e62e3888ebbd1c25b0e07435049
bemo-codes <shivamxapp@gmail.com>
2026-08-24 21:33:29 +0530

    Add files via upload

---
+++ Assignment38_6.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def main():
+    df = pd.read_csv("student_performance_ml.csv")
+
+    print(df.head())
+    print(df.tail())
+    print(df.shape)
+    print(df.columns)
+    print(df.columns.dtype)
+
+    total_students = len(df)
+    passed_students = (df["FinalResult"] == 1).sum()
+    failed_student = (df["FinalResult"] == 0).sum()
+
+    print("Total number of students: ", total_students)
+    print("Passed students: ", passed_students)
+    print("Failed students: ", failed_student)
+
+    print("Average study hours: ", (df["StudyHours"]).mean())
+    print("Average attendance: ", (df["Attendance"]).mean())
+    print("Maximum Previous Score", (df["PreviousScore"].max()))
+    print("Maximum Sleep Hours: ", (df["SleepHours"].max()))
+
+    print(df["FinalResult"].value_counts(normalize=True)*100)
+
+    #Higher Study Hours increase the chances of passing as you observe the student who've studied above 4 hr mark have all passed so putting in more study hours is more beneficial over attendance.
+    
+    df["StudyHours"].hist(bins=10, edgecolor = 'black', alpha = 0.8, rwidth = 0.7)
+
+    plt.title("Study Hours Distribution")
+    plt.xlabel("Study Hours")
+    plt.ylabel("Frequency")
+    plt.grid(False)
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
